fb4e02ee9ae8b1226a2e65cbcc5988b9b44923cd
Cesar Angeles <cangeles.santiago@gmail.com>
2024-04-13 11:54:07 -0600

    Fix secret

--- microservice/connection_DB.js
+++ microservice/connection_DB.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host:process.env.DB_HOST || 'localhost',
     user:process.env.DB_USER || 'root',
-    password:process.env.DB_PASSWORD || 'D4t4b4s3$2024',
+    password:process.env.DB_PASSWORD || '',
     database:process.env.DB_NAME || 'my_store',
 })
 
